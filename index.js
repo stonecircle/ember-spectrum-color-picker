a9faebbd36144a190a92eeb04fdd4d76e56502fc
Timmy O'Mahony <hey@timmyomahony.com>
2017-06-11 13:22:49 +0100

    Added ember changes

--- index.js
+++ index.js
@@ -2,13 +2,31 @@
 'use strict';
 
 var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+var map = require('broccoli-stew').map;
 
 var defaults = {
-  assetPath: 'bower_components/spectrum'
+  assetPath: 'bower_components/spectrum',
 };
 
 module.exports = {
   name: 'ember-spectrum-color-picker',
+
+  treeForVendor(defaultTree) {
+
+    var spectrumTree = new Funnel(
+      path.join(this.project.root, 'bower_components', 'spectrum'),
+      { files: ['spectrum.js'] }
+    );
+
+    spectrumTree = map(
+      spectrumTree,
+      content => `if (typeof FastBoot === 'undefined') { ${content} }`
+    );
+
+    return defaultTree ? new MergeTrees([defaultTree, spectrumTree]) : spectrumTree;
+  },
 
   included: function(app) {
     this._super.included(app);
@@ -17,10 +35,8 @@ module.exports = {
 
     var assetPath = options.assetPath || defaults.assetPath;
 
-    if (!process.env.EMBER_CLI_FASTBOOT) {
-      app.import(path.join(assetPath, 'spectrum.js'));
-    }
-
+    app.import('vendor/spectrum.js');
     app.import(path.join(assetPath, 'spectrum.css'));
-  }
+  },
+
 };
